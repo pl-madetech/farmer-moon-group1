bff68e3e54179e3bfb59b77765bb5bbd00e8e40d
paulo.lancao <paulo.lancao@madetech.com>
2021-01-13 14:43:58 +0000

    testing deployment new code

--- src/browser/App.tsx
+++ src/browser/App.tsx
@@ -5,6 +5,7 @@ import useConfig from "../components/useConfig";
 import { StaticRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 import Login from "../components/login";
+import Home from "../components/home";
 
 /**
  * Our Web Application
@@ -14,35 +15,22 @@ export default function App() {
   return (<Router>
     <div className="App">
       <nav className="navbar navbar-expand-lg navbar-light fixed-top">
+      <h1 className="App-title">{config.app.TITLE}</h1>
         <div className="container">
-          <Link className="navbar-brand" to={"/sign-in"}>RemoteStack</Link>
           <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
             <ul className="navbar-nav ml-auto">
               <li className="nav-item">
                 <Link className="nav-link" to={"/sign-in"}>Sign in</Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to={"/sign-up"}>Sign up</Link>
               </li>
             </ul>
           </div>
         </div>
       </nav>
 
-      <div className="App">
-      <header className="App-header">
-        <h1 className="App-title">Welcome to {config.app.TITLE}</h1>
-      </header>
-      <p className="App-intro">
-        I have changed the app
-        Another change
-      </p>
-    </div>
-
       <div className="outer">
         <div className="inner">
           <Switch>
-            <Route exact path='/' component={Login} />
+            <Route exact path='/' component={Home} />
             <Route path="/sign-in" component={Login} />
           </Switch>
         </div>
---
+++ src/components/home.tsx
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+export default class Home extends Component {
+    render() {
+        return (
+            <h3>Hi Farmer Moon are you ready to start your journey !!!</h3>
+        );
+    }
+}
