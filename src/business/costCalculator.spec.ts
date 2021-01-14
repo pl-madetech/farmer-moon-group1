ba4b551c7234ce93ecb0ea687eecad3df4b0dc56
paulo.lancao <paulo.lancao@madetech.com>
2021-01-14 12:07:30 +0000

    Fix tests of cals

--- src/business/costCalculator.spec.ts
+++ src/business/costCalculator.spec.ts
@@ -2,12 +2,12 @@ import { calculateCost } from "./costCalculator";
 
 describe('costCalculator', () => {
     test('integer corn amount', () => {
-        expect(calculateCost({corn: 3, goose: 0})).toBe(75);
+        expect(calculateCost({corn: 3, goose: 0})).toBe(150);
     });
     test('non-integer corn amount less than x.5', () => {
-        expect(calculateCost({corn: 3.4, goose: 0})).toBe(75);
+        expect(calculateCost({corn: 3.4, goose: 0})).toBe(150);
     });
     test('non-integer corn amount more than x.5', () => {
-        expect(calculateCost({corn: 3.6, goose: 0})).toBe(75);
+        expect(calculateCost({corn: 3.6, goose: 0})).toBe(200);
     });
 });
